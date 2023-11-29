b333f72cce32a0452efa0f812de374d7b8b70ed0
Greg Hellings <greg.hellings@gmail.com>
2023-11-28 19:02:33 -0600

    Do not write to non-existent folder

--- distros/debian/9/finalize.sh
+++ distros/debian/9/finalize.sh
@@ -4,11 +4,5 @@ set -ex
 apt update
 apt upgrade -y
 
-# Configure sshd to be faster
-cat << EOF > /etc/ssh/sshd_config.d/00-vagrant_accept_pubkey.conf
-UseDNS no
-PubkeyAcceptedKeyTypes +ssh-rsa
-EOF
-
 # Clean up after ourselves
 apt-get clean
